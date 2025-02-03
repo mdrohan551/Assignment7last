0fa2bbc54052907e04e91c5cc2e22900bcde2ee9
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-02-04 01:04:32 +0600

    Update api.js

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -12,87 +12,41 @@ router.post("/login", Controller.loginUser);
 //user Logout Cookie deleted
 router.get("/logout", AuthMiddleware, Controller.logoutUser);
 // File upload multer
-router.post(
-  "/upload-file-multer",
-  AuthMiddleware,
-  uploads.single("file"),
-  Controller.uploadMulterAvatar
-);
+router.post('/upload-file-multer',AuthMiddleware,uploads.single('file'),Controller.uploadMulterAvatar);
 
-//Create Blog
-router.post(
-  "/create-blog",
-  AuthMiddleware,
-  SectionController.CreateBlogController
-);
-router.post(
-  "/update-blog/:id",
-  AuthMiddleware,
-  SectionController.UpdateBlogController
-);
-router.get(
-  "/delete-blog/:id",
-  AuthMiddleware,
-  SectionController.DeleteBlogController
-);
 
-router.get("/read-blog", SectionController.ReadBlogController);
+//Create Blog 
+router.post('/create-blog',AuthMiddleware,SectionController.CreateBlogController);
+router.post('/update-blog/:id',AuthMiddleware,SectionController.UpdateBlogController);
+router.get('/delete-blog/:id',AuthMiddleware,SectionController.DeleteBlogController);
+
+router.get('/read-blog',SectionController.ReadBlogController);
 //get blog for homepage
-router.get("/read-blog-home", SectionController.GetBlogHomeController);
-router.get(
-  "/read-single-blog/:slug",
-  SectionController.ReadSingleBlogController
-);
+router.get('/read-blog-home',SectionController.GetBlogHomeController);
+router.get('/read-single-blog/:slug',SectionController.ReadSingleBlogController);
 
 // Create Slider
-router.post(
-  "/create-slider",
-  AuthMiddleware,
-  SectionController.CreateSliderController
-);
+router.post('/create-slider',AuthMiddleware,SectionController.CreateSliderController);
 
-router.get("/read-slider", SectionController.ReadSliderController);
+router.get('/read-slider',SectionController.ReadSliderController);
 
-// Create Team
-router.post(
-  "/create-team",
-  AuthMiddleware,
-  SectionController.CreateTeamController
-);
-router.post(
-  "/update-team/:id",
-  AuthMiddleware,
-  SectionController.UpdateTeamController
-);
-router.get(
-  "/delete-team/:id",
-  AuthMiddleware,
-  SectionController.DeleteTeamController
-);
+// Create Team 
+router.post('/create-team',AuthMiddleware,SectionController.CreateTeamController);
+router.post('/update-team/:id',AuthMiddleware,SectionController.UpdateTeamController);
+router.get('/delete-team/:id',AuthMiddleware,SectionController.DeleteTeamController);
 
-router.get("/read-team", SectionController.ReadTeamController);
+router.get('/read-team',SectionController.ReadTeamController);
 
 //Create Services
-router.post(
-  "/create-service",
-  AuthMiddleware,
-  SectionController.CreateServiceController
-);
-router.get(
-  "/delete-service/:id",
-  AuthMiddleware,
-  SectionController.DeleteServiceController
-);
+router.post('/create-service',AuthMiddleware,SectionController.CreateServiceController);
+router.get('/delete-service/:id',AuthMiddleware,SectionController.DeleteServiceController);
 
-router.get("/read-service", SectionController.ReadServiceController);
+router.get('/read-service',SectionController.ReadServiceController);
 
 // Create Contact
-router.post("/create-contact", SectionController.CreateContactController);
+router.post('/create-contact',SectionController.CreateContactController);
 
-router.get(
-  "/read-contact",
-  AuthMiddleware,
-  SectionController.ReadContactController
-);
+router.get('/read-contact',AuthMiddleware,SectionController.ReadContactController);
+
 
 export default router;
